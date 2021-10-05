6b1222fbf895a85800593a21bde91ce8c9148a87
Arfameher <arfaameher@outlook.com>
2021-10-05 17:26:14 +0200

    Need to correct errors

--- utils/game.py
+++ utils/game.py
@@ -1,20 +1,52 @@
-from typing import List
+import typing
+from card import *
+from player import *
+
+
 class Board():
 
-    def __init__(self, players: List[str]):
-        self.players = players
-        turn_count 
-        active_cards
-        history_cards
+    def __init__(self):
+        self.players = []
+        self.turn_count = 0
+        self.active_cards = []
+        self.history_cards = []
 
     def start_game(self):
 
+        self.players = []
         print("Game is starting !! ")
         n = int(input("Enter number of Players : "))
         i = 1 
         while i <= n:
-            Players[i] = input("Enter Player {} name : ".format(i))
+            name = input("Enter Player {} name : ".format(i))
+            self.players.append(Player(name))
             i += 1
+
+        # call fill_deck()
+        deck = Deck()
+        deck.fill_deck()
+        deck.shuffle()
+        deck.distribute(self.players)
+        x1 = 52 // len(self.players)
+        for i in range(0, x1):
+            self.active_cards = []
+            for j in self.players:
+                card = j.play()
+                self.active_cards.append(card)
+                j.cards.remove(card)
+                j.turn_count += 1
+            self.history_cards.append(self.active_cards)
+            self.turn_count += 1
+            print(f"Turn count: {self.turn_count}")
+            print(f"Cards played this turn: {self.active_cards}")
+            print(f"Number of cards played in the game : {self.history_cards}")
+
+        print("No cards left !!")
+
+    def __str__(self) -> str:
+        return f"Player: {self.players}, Turn: {self.turn_count}, Cards in hand: {self.active_cards}"
+
+board = Board()
+board.start_game()
         
-        
-    def __str__(self):
+
--- utils/player.py
+++ utils/player.py
@@ -1,38 +1,63 @@
-import Card from card.py
-import random, itertools
+from card import *
+import random
+from typing import List
+import typing
 
 class Player:
 
-    def __init__(self, name, cards, turn_count, number_of_cards, history):
-        turn_count = 0
-        number_of_cards = 0
+    def __init__(self, name):
         self.history = []
         self.name = name
-        self.cards = cards
-        
+        self.cards = []
+        self.turn_count = 0
+        self.number_of_cards = 0
 
     def play(self):
-        card = random.shuffle(cards)
+        number = int(input(f"\n{self.name}, you have {len(self.cards)} cards left, choose the card to be played : "))
+        card = self.cards[number]
         self.history += [card]
-        
+        print(f"{self.name} Turn:{self.turn_count} played : {card.value} {card.icon}")
+        return card 
 
     def __str__(self):
-
+        return f"Name: {self.name} Turn counts: {self.turn_count} Number of Cards: {self.number_of_cards} Cards played: {self.history}"
 
 
 class Deck():
 
-    def __init__(self, cards):
-        self.cards = []
+    def __init__(self):
+        self.cards = []  # cards contains LIST OF INSTANCES OF "Card"
 
     def fill_deck(self):
-        cards = list(itertools.product(icon,value))
+        icon = "♥, ♦, ♣, ♠".split()
+        value = "A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K".split()
+        for i in icon:
+            for j in value:
+               deck = Card(i,j)  
+               self.cards.append(deck)   # cards should contain "52 cards at the end"
+
+        #  self.cards = list(itertools.product(icon,value))
 
     def shuffle(self):
-        deck = random.shuffle(cards)
+        random.shuffle(self.cards)
 
-    def distribute(self,player):
+    def distribute(self, players):
+        x = 52 // len(players)
+        rem = 52 % len(players)
+        for player in players:
+            for i in range(0,x):
+                player.cards.append(self.cards[0])
+                self.cards.remove(self.cards[0])
+
+        print("Cards remaining after distributing : ",end = "")
+        if rem != 0:
+            for i in range(0,rem):
+                print(self.cards[0],end = " ")
+                self.cards.remove(self.cards[0])
+        else:
+            print("None.")
 
     def __str__(self):
+        return f"Cards in hand: {self.cards}"
 
 
